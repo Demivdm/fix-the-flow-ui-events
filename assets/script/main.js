726127b80b5d4c4498ccfd8811d8ab714ffb1e68
Demivdm <demi.van.der.maarl@hva.nl>
2022-12-14 17:23:21 +0100

    interactie toegevoegd

--- assets/script/main.js
+++ assets/script/main.js
@@ -4,6 +4,8 @@ let interaction = document.querySelector('a:nth-of-type(12)')
 var kleur = document.getElementById('kleur')
 var draaien = document.getElementById('draaien')
 
+
+
 interaction.addEventListener('click', jumpHandler)
 interaction.addEventListener('animationend', jumpHandler)
 
@@ -11,16 +13,34 @@ function jumpHandler() {
   interaction.classList.toggle('jump')
 }
 
-//interactie1
+// interactie 1
 
-kleur.addEventListener('mouseover', function handleMouseOver() {
+  kleur.addEventListener('mouseover', function handleMouseOver() {
   kleur.classList.add('achtergrond');
 });
 
-kleur.addEventListener('mouseout', function handleMouseOut() {
+  kleur.addEventListener('mouseout', function handleMouseOut() {
   kleur.classList.remove('achtergrond');
 });
 
 
 
+// interactie 2
+draaien.addEventListener('click', turnHandler)
+draaien.addEventListener('animationend', turnHandler)
+
+function turnHandler() {
+  draaien.classList.toggle('gedraaid')
+}
+
+// interactie 3
+
+geschud.addEventListener('click', shakeHandler)
+geschud.addEventListener('animationend', shakeHandler)
+
+function shakeHandler() {
+  geschud.classList.toggle('schudden')
+}
+
+// interactie 4
 
