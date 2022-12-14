c7532e31aa3af228b2ef844e1d384cabd37c4bf9
Demivdm <demi.van.der.maarl@hva.nl>
2022-12-14 10:20:35 +0100

    js toegevoegd

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,8 +1,22 @@
 let interaction = document.querySelector('a:nth-of-type(12)')
 
+var kleur = document.getElementById('kleur')
+var draaien = document.getElementById('draaien')
+
 interaction.addEventListener('click', jumpHandler)
 interaction.addEventListener('animationend', jumpHandler)
 
 function jumpHandler() {
   interaction.classList.toggle('jump')
+}
+
+function KleurVeranderen(){  
+  
+  kleur.style.backgroundColor='Red';
+   
+}
+
+function TurnAround() {
+
+  draaien.classList.add("turnover")
 }
