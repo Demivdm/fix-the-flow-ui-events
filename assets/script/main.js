245605396cae1852559602be1099fe644c337277
Demivdm <demi.van.der.maarl@hva.nl>
2023-01-07 14:11:56 +0100

    interacties toegevoegd
    interactie 8,9 en 10 toegevoegd

--- assets/script/main.js
+++ assets/script/main.js
@@ -9,6 +9,9 @@ let interface = document.querySelector('a:nth-of-type(10)')
 let feedback = document.querySelector('a:nth-of-type(15)')
 let feedforward = document.querySelector('a:nth-of-type(16)')
 let and = document.querySelector('a:nth-of-type(3)')
+let dev = document.querySelector('a:nth-of-type(4)')
+let events = document.querySelector('a:nth-of-type(11)')
+let states = document.querySelector('a:nth-of-type(18)')
 
 interaction.addEventListener('click', jumpHandler)
 interaction.addEventListener('animationend', jumpHandler)
@@ -81,4 +84,31 @@ and.addEventListener('animated', skinnyHandler)
 
 function skinnyHandler(){
   and.classList.toggle('skinny')
+}
+
+//interactie 8
+
+dev.addEventListener('click', colorHandler)
+dev.addEventListener('animated', colorHandler)
+
+function colorHandler(){
+  dev.classList.toggle('color')
+}
+
+//interactie 9
+
+events.addEventListener('click', shadowHandler)
+events.addEventListener('animated', shadowHandler)
+
+function shadowHandler(){
+  events.classList.toggle('shadow')
+}
+
+//interactie 10
+
+states.addEventListener('click', boxHandler)
+states.addEventListener('animated', boxHandler)
+
+function boxHandler(){
+  states.classList.toggle('box')
 }
